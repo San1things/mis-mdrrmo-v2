d50984653dcf9a57e76b7bfbecb03bc0ab24f63c
San1things <businesslikesan1@gmail.com>
2024-08-20 21:34:01 +0800

    design user notif

--- database/migrations/2024_08_17_035640_tbl_notif.php
+++ database/migrations/2024_08_17_035640_tbl_notif.php
@@ -14,8 +14,11 @@ return new class extends Migration
         Schema::create('tbl_notif', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
+            $table->string('user_type');
             $table->string('title');
             $table->string('description');
+            $table->string('link')->nullable()->default('#');
+            $table->boolean('seen')->default(false);
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('tbl_users')->onDelete('cascade');
--- resources/views/user/usernotif.blade.php
+++ resources/views/user/usernotif.blade.php
@@ -1,8 +1,34 @@
 @extends('user.components.userlayout')
 @section('resident_content')
+    <div class="user-notif">
+        <div class="container-xl">
+            <div class="user-notif-title">
+                <h2>Notifications</h2>
+            </div>
+
+            <div class="user-notif-content">
+                @for ($i = 0; $i < 30; $i++)
+                    <a class="remove-design-notif" href="">
+                        <div class="notif-banner">
+                            <h6>Sample Title Notification</h6>
+                            <p>sample notification description</p>
+                            <p class="notif-date">date sample</p>
+
+                            <div class="btn-group san1-notif-settings">
+                                <a class="btn dropdown-toggle fs-3 pe-3" data-bs-toggle="dropdown" type="button"
+                                    aria-expanded="false">
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-end">
+                                    <li><button class="dropdown-item fs-4" type="button">Remove Notification</button></li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </a>
+                @endfor
 
 
-
-
-
+            </div>
+        </div>
+    </div>
 @endsection
